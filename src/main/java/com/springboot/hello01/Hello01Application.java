32b9f87884404374ecba9b0fb3a2db434fe5e18b
minkyoung kim <minry20415@naver.com>
2022-10-31 14:48:08 +0900

    [add] UserDao 연습

---
+++ src/main/java/com/springboot/hello01/Hello01Application.java
@@ -0,0 +1,13 @@
+package com.springboot.hello01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hello01Application.class, args);
+    }
+
+}
