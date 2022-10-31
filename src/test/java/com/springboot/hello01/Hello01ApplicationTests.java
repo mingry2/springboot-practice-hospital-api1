f60f6c4bd34475564fbb1ffebc03f5b9b455b25a
minkyoung kim <minry20415@naver.com>
2022-10-31 14:48:15 +0900

    [add] UserDao 연습

---
+++ src/test/java/com/springboot/hello01/Hello01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.hello01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
