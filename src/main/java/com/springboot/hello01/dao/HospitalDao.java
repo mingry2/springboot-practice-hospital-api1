ae19a5ec2e1f17a696160e9b13908ed85fbf77f2
minkyoung kim <minry20415@naver.com>
2022-11-01 13:03:33 +0900

    [update] SQL문 JdbcTemplate으로 만들기 -> Database랑 연결

--- src/main/java/com/springboot/hello01/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello01/dao/HospitalDao.java
@@ -1,4 +1,34 @@
 package com.springboot.hello01.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HospitalDao {
+    //(1, hospital.getId()); // col:0
+    //("의원", hospital.getOpenServiceName()); // col:1
+    //(3620000,hospital.getOpenLocalGovernmentCode()); // col:3
+    //("PHMA119993620020041100004",hospital.getManagementNumber()); // col:4
+    //(LocalDateTime.of(1999, 6, 12, 0, 0, 0), hospital.getLicenseDate()); // col:5
+    //(1, hospital.getBusinessStatus()); // col:7
+    //(13, hospital.getBusinessStatusCode()); // col:9
+    //("062-515-2875", hospital.getPhone()); // col:15
+    //("광주광역시 북구 풍향동 565번지 4호 3층", hospital.getFullAddress()); // col:18
+    //("광주광역시 북구 동문대로 24, 3층 (풍향동)", hospital.getRoadNameAddress()); // col:19
+    //("효치과의원", hospital.getHospitalName()); // col:21
+    //("치과의원", hospital.getBusinessTypeName()); // col:25
+    //(1, hospital.getHealthcareProviderCount()); // col:30
+    //(0, hospital.getPatientRoomCount()); // col:31
+    //(0, hospital.getTotalNumberOfBeds()); // col:32
+    //(52.29, hospital.getTotalAreaSize()); // col:33
+    private final JdbcTemplate jdbcTemplate;
+
+    public HospitalDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void add(HospitalDao hospitalDao) {
+        String sql = "INSERT INTO `likelion-db1`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        this.jdbcTemplate.update(sql);
+    }
 }
